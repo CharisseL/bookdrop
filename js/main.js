b84806b7a3e119a3940f29ebf30b6fd1c8b7904b
CharisseL <charisse8@gmail.com>
2014-12-07 21:36:36 -0600

    fixed dependencies install

---
+++ js/main.js
@@ -0,0 +1,32 @@
+;(function (){
+	'use strict';
+
+	angular.module('bookDropApp', []
+		.controller('BookController', function(){
+			var addBook = this;
+
+			addBook.books =[
+-        {
+-          title: 'The Warmth of Other Suns,
+-          author: 'Isabelle Wilkerson'
+-          year: '2010',
+-          isbn: '4578384738',
+-          image: 'http://i.imgur.com/IJMGmz8.png'
+-        },
+-        {
+-          title: 'HTML & CSS',
+-          author: 'John Duckett',
+-          year: '2014',
+-          isbn: '1290394586',
+-          image: 'http://i.imgur.com/4NPS39X.jpg'
+-        },
+-        {
+-          name: 'The Book of Basketball',
+-          desc: 'Bill Simmons',
+-          due: '2008',
+-          isbn: '0987654321',
+-          image: 'http://i.imgur.com/VDHV0KT.jpg'
+-        }
+-      ];
+						
+				
